24cfa7719168f5de06cf57e6dedebc2c010e9722
fiddlefordstan <cmzosia@gmail.com>
2024-11-08 12:09:30 +0100

    Update zadanie4.py

---
+++ zadanie4.py
@@ -0,0 +1,8 @@
+# Pobranie ciągu znaków od użytkownika
+ciag = input("Podaj ciąg znaków: ")
+
+# Odwrócenie ciągu znaków
+odwrocony_ciag = ciag[::-1]
+
+# Wyświetlenie odwróconego ciągu
+print("Odwrócony ciąg: ", odwrocony_ciag)
